af0b3696577221be3bc51ce25923e94ec4cc16ed
jose vitor <josevitorrodrigues17@gmail.com>
2025-06-04 01:34:21 -0300

    feat: setup api, load configurations

---
+++ backend/internal/api/auth_handler.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/josevitorrodriguess/any-song/backend/internal/models"
+)
+
+func (api *API) SignInHandler(c *fiber.Ctx) error {
+	var idToken string
+	if err := c.BodyParser(&idToken); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Dados inválidos",
+		})
+	}
+
+	if idToken == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Token de autenticação é obrigatório",
+		})
+	}
+
+	decodedToken, err := api.Auth.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Token inválido",
+		})
+	}
+	userEmail := decodedToken.Claims["email"].(string)
+
+	user, err := api.UserService.GetUserByEmail(userEmail)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Erro ao verificar usuário",
+		})
+	}
+	if user == nil {
+		api.UserService.CreateUser(&models.User{
+			Email:          userEmail,
+			Name:           decodedToken.Claims["name"].(string),
+			ProfilePicture: decodedToken.Claims["picture"].(string),
+			IsActive:       true,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"valid": true,
+	})
+}
+
+func (api *API) LogoutHandler(c *fiber.Ctx) error {
+	user, exists := GetUserFromContext(c)
+	if !exists {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Usuário não encontrado",
+		})
+	}
+
+	err := api.Auth.RevokeRefreshTokens(context.Background(), user.UID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Erro ao fazer logout",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Logout realizado com sucesso",
+	})
+}
